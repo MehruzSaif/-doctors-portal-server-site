dc11dedd292766828a9ef121937f2cab5c1ef57a
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-16 07:04:37 +0600

    used forEach in services

--- index.js
+++ index.js
@@ -12,46 +12,66 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxvr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
+
 async function run() {
-    try{
-        await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
-        const bookingCollection = client.db('doctors_portal').collection('bookings');
+  try {
+    await client.connect();
+    const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('bookings');
 
-        // to load service data on appointment
-        app.get('/service', async(req, res) => {
-          const query = {};
-          const cursor = serviceCollection.find(query);
-          const services = await cursor.toArray();
-          res.send(services);
-        });
+    // to load service data on appointment
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        /**
-         * API Naming Convention
-         * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
-         * app.get('/booking/:id') // get a specific booking 
-         * app.post('/booking') // add a new booking
-         * app.patch('/booking/:id) // update a specific booking
-         * app.delete('/booking/:id) // delete a specific booking
-         */
+    app.get('/available', async (req, res) => {
+      const date = req.query.date || 'May 11, 2022';
 
 
-        app.post('/booking', async(req, res) => {
-          const booking = req.body;
-          const result = await bookingCollection.insertOne(booking);
-          res.send(result);
-        })
+      // step 1: Get all services
 
-    }
-    finally{
+      const services = await serviceCollection.find().toArray();
 
-    }
+      // set 2: Get the booking of that day
+      const query = {date: date};
+      const bookings = await bookingCollection.find(query).toArray();
+
+      res.send(bookings);
+    })
+
+    /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) // update a specific booking
+     * app.delete('/booking/:id) // delete a specific booking
+     */
+
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists })
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    })
+
+  }
+  finally {
+
+  }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Doctors Portal!')
 })
 
 app.listen(port, () => {
