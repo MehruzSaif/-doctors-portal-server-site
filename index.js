337baebb0b5da248cb4bb4c7128e1d4d352c6934
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-17 06:06:07 +0600

    unauthorized access token (401) and forbidden access (403) have been used by authHeader

--- index.js
+++ index.js
@@ -17,8 +17,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({message: 'UNAUTHORIZED access'});
+    return res.status(401).send({message: 'UNAUTHORIZED Access'});
   }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+
+    if(err) {
+      return res.status(403).send({message: 'Forbidden Access'})
+    }
+    res.decoded = decoded;
+    next();
+  });
 }
 
 async function run() {
